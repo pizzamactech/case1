b1475f6985de95f93017c9e039043558524136ca
pizzamactech <81186887+pizzamactech@users.noreply.github.com>
2021-04-09 22:08:17 -0400

    Initial commit

---
+++ tc_cart.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: 
+   Date:   
+   
+   Filename: tc_cart.js
+	
+  
+*/
+var cartHTML =
+'<tr><td><img src='tc_ item .png' alt=' item ' /></td>' +
+'<td> description </td>' +
+'<td>$ price </td>' +
+'<td> quantity </td>' +
+'<td>$ cost </td></tr>'
+'<tr>' +
+'<td colspan='4'>Subtotal</td>'+
+'<td>$ total </td>'+
+'</tr>' +
+'</table>'
+var total =
+orderTotal + itemCost
+var itemCost = 
+price(itemCost) * quanity(itemQty)
---
+++ tc_order.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+  
+   Author: 
+   Date:   
+     
+   Filename: tc_order.js  
+
+  /*The item array contains the ID numbers of the items ordered by the customer*/
+   var item = [10582,23015,41807,10041];
+   /*The itemDescription array contains the description of each item*/
+   var itemDescription = ["1975 Green Bay Packers Football (signed)",item 10582,
+   "Tom Landry 1955 Football Card (unsigned)",item 23015,
+   "1916 Army-Navy Game, Framed Photo (signed)", Item 41807,
+   "Protective Card Sheets",Item 10041];
+   /*The itemPrice array contains the price of each item*/
+   var itemPrice = [149.93, 89.98, 334.93,22.67];
+   /*The itemQty array contains the quantity ordered of each item*/
+   var itemQty = [1,1,1,4];
+*/
+ 
